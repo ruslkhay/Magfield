6a817e55abed093a0adf29ae652bca976034687d
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-11-10 19:44:19 +0300

    Preserve previous work

--- src/magfield/forecast/algorithms.py
+++ src/magfield/forecast/algorithms.py
@@ -13,72 +13,64 @@ class EDF:
 
     from typing import List
 
-    def __init__(self, window: List[int], K: int) -> None:
+    def __init__(self, window: List[int], M: int = 0, ord_quant: int = 0) -> None:
         """Generate class object.
 
         :param window: length and step of floating window.
         :param K: Number of grid points
         """
-        self.K = K
+        self.quants = []
+        self.probs = []
+        self.M = M
+        self.quant_probs = np.linspace(0, 1, ord_quant, endpoint=False)[1:]
         self.window = {"length": window[0], "step": window[1]}
 
     def fit(self, input):
-        from numpy.typing import NDArray
         from tqdm.notebook import tqdm
         from scipy.stats import ecdf
         import numpy as np
+        from scipy.stats.mstats import mquantiles
 
-        def make_grid(series: NDArray, h) -> NDArray:
-            """Construct grid to evaluating empirical distribution function."""
-            x_K = series[-1]
-            grid = series[::h]
-            if len(grid) < self.K:
-                grid = np.append(grid, x_K)
-            return grid
-
-        h = (self.window["length"] - 1) // (self.K - 1)
         i = 0
-        quants = []
         # Main window shifting part
         for i in tqdm(range(len(input) - self.window["length"] + 1)):
             counts = input[i : self.window["length"] + i + 1]
-            window = ecdf(counts)
-            # order_stats = window.cdf.quantiles
-            T = np.apply_along_axis(window.cdf.evaluate, 0, counts[::h]).tolist()
-            # T = np.apply_along_axis(window.cdf.evaluate, 0, grid)
-            quants.append(T)
-            i += self.window["step"]
-        self.T = np.array(quants)
+            if self.quant_probs.size > 0:
+                self.quants.append(list(mquantiles(counts, prob=self.quant_probs)))
+            if self.M:
+                distrib_func = ecdf(counts).cdf
+                prob = np.linspace(0, 1, self.M, endpoint=False)[1:]
+                quants = mquantiles(counts, prob=prob)
+                self.probs.append(list(distrib_func.evaluate(quants)))
 
 
-class Quant:
-    """Make forecast based on quantiles."""
+# class Quant:
+#     """Make forecast based on quantiles."""
 
-    from typing import List
+#     from typing import List
 
-    def __init__(self, window: List[int], order: int) -> None:
-        """Generate class object.
+#     def __init__(self, window: List[int], order: int) -> None:
+#         """Generate class object.
 
-        :param window: length and step of floating window.
-        :param order: Order of quantiles. For example if `order=10` then
-        we have business with deciles.
-        """
-        self.order = order
-        self.window = {"length": window[0], "step": window[1]}
+#         :param window: length and step of floating window.
+#         :param order: Order of quantiles. For example if `order=10` then
+#         we have business with deciles.
+#         """
+#         self.order = order
+#         self.window = {"length": window[0], "step": window[1]}
 
-    def fit(self, input):
-        from tqdm.notebook import tqdm
-        from scipy.stats.mstats import hdquantiles
-        from scipy.stats import ecdf
+#     def fit(self, input):
+#         from tqdm.notebook import tqdm
+#         from scipy.stats.mstats import hdquantiles
+#         from scipy.stats import ecdf
 
-        probs = np.linspace(0, 1, self.order)
-        quants = []
-        i = 0
-        # Main window shifting part
-        for i in tqdm(range(len(input) - self.window["length"] + 1)):
-            counts = input[i : self.window["length"] + i]
-            window = ecdf(counts)
-            order_stats = window.cdf.quantiles
-            quants.append(hdquantiles(order_stats, prob=probs))
-            i += self.window["step"]
-        self.T = np.array(quants)
+#         probs = np.linspace(0, 1, self.order)
+#         quants = []
+#         i = 0
+#         # Main window shifting part
+#         for i in tqdm(range(len(input) - self.window["length"] + 1)):
+#             counts = input[i : self.window["length"] + i]
+#             order_stats = window.cdf.quantiles
+#             quants.append(hdquantiles(order_stats, prob=probs))
+#             i += self.window["step"]
+#         self.T = np.array(quants)
